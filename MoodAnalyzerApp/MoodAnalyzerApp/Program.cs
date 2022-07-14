ed6d39595e71c98d068c90f660535042c1df59bd
akshayb407 <akshayb407@gmail.com>
2022-07-14 13:02:25 +0530

    Master added

--- MoodAnalyzerApp/MoodAnalyzerApp/Program.cs
+++ MoodAnalyzerApp/MoodAnalyzerApp/Program.cs
@@ -6,7 +6,7 @@ namespace MoodAnalyzerApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Mood Analyzer");
         }
     }
 }
